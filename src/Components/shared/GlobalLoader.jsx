7466ee0324e5ceefb818157bf9e52d0b3e97ceb3
Johnny Mujito <106999374+Mangunza@users.noreply.github.com>
2026-04-24 20:11:18 +0100

    feat: atualização do frontend (lazy loading + melhorias)

---
+++ src/Components/shared/GlobalLoader.jsx
@@ -0,0 +1,13 @@
+const GlobalLoader = () => {
+  return (
+    <div style={{
+      width: "100%",
+      padding: "2rem",
+      textAlign: "center"
+    }}>
+      ⏳ Carregando...
+    </div>
+  );
+};
+
+export default GlobalLoader;
